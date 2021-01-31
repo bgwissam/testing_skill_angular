b17e315a8364c6030a37a5cf3c88a8c9a556f16f
Wissam Bou Ghannam <bgwissam@hotmail.com>
2021-01-31 21:20:48 +0300

    adding web api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
 
-  constructor(private messageService: MessageService) { }
+  private heroUrl = 'api/heroes'; // Url for web api
+
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add(`List of selected Heroes:`);
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroUrl);
   }
 
+  getHero(id: string): Observable<Hero | undefined> {
+    this.messageService.add(`Chosen Hero id: ${id}`);
+    return of(HEROES.find((hero) => hero.id === parseInt(id)));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 }
